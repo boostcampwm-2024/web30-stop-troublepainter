832ae8413d2ee1bf629ee5fffc98a21315f67719
Taeyeon Yoon <okko8522@gmail.com>
2024-11-07 13:32:17 +0900

    Revert "feat: Implement main logo component (#18)"
    This reverts commit d41ba4b6b0f6daef8b652ec179e2f950b704f190.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,10 @@
 import './App.css';
 import helpIcon from '@/assets/help-icon.svg';
 import { Button } from '@/components/ui/Button';
-import { Logo } from '@/components/ui/Logo';
 
 const App = () => {
   return (
     <>
-      <Logo />
-      <Logo variant="side" />
-
       <h1 className="transition">Hello world!</h1>
       <Button className="font-neodgm-pro text-2xl font-normal">유미라</Button>
       <Button variant="transperent" size="icon">
@@ -17,5 +13,4 @@ const App = () => {
     </>
   );
 };
-
 export default App;
